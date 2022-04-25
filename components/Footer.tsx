1fdeee897b275171d9bd183083596fb6f0eccddd
Juraj Marcin <juraj@jurajmarcin.com>
2022-04-25 20:17:50 +0200

    Add Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from '../styles/Footer.module.css';
+
+const Footer = () => (
+  <footer className={styles.footer}>
+    Made with &lt;3 by Juraj Marcin 2022
+  </footer>
+);
+
+export default Footer;
